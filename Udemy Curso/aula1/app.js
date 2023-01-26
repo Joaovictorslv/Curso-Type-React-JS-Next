acf31ef31bce62ecc3835cf871a27a9697777204
João Victor da Silva <83296570+Joaovictorslv@users.noreply.github.com>
2023-01-26 12:01:39 -0300

    Add files via upload

---
+++ Udemy Curso/aula1/app.js
@@ -0,0 +1,6 @@
+console.log('Hello World!');
+console.log('O futuro é pika!');
+/* Tive um pequeno problema coma exibição do console.log no Output, porém 
+vi que era para dar F5 e conseguir debugar o código mas acabaria tendo que
+fazer esse processo sempre. A partir disso configurei o Code Runner para 
+salvar os arquivos automaticamente funcionouuuu!*/
---
+++ Udemy Curso/aula7/index.js
@@ -0,0 +1,18 @@
+console.log('Johnny'); //string
+console.log("Johnny");
+
+console.log(654); //Number
+console.log(231, 789.56, 5, 87);
+
+console.log(654, 78.23, "9", "Johnny");
+
+/* 
+
+"", '', ``, pois é tipo string 
+
+"Luis 'Otavio'" Ok
+'Luis "Otavio"' Ok
+`Luis "Otavio"` Ok
+Dois iguais não pode
+
+*/

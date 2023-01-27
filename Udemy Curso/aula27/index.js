9e0a161d30f80d5fd06f0d8b34dc7340187fd8a0
João Victor da Silva <83296570+Joaovictorslv@users.noreply.github.com>
2023-01-27 09:27:15 -0300

    Add files via upload

---
+++ Udemy Curso/aula27/index.js
@@ -0,0 +1,10 @@
+let num = 9.578;
+let num1 = Math.floor(num); //para baixo
+let num2 = Math.ceil(num); //para cima
+let num3 = Math.round(num); //cima
+
+console.log(num);
+console.log(num1);
+console.log(num2);
+console.log(num3);
+
---
+++ Udemy Curso/aula7/index1.js
@@ -0,0 +1,3 @@
+let nome = "johnny";
+let hora = 6;
+console.log('Meu nome é', nome ,"e acordo às", hora);
